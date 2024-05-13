763188fbad97c0972aec7e33ebb2695b6ce9fd2a
Brunogabriel12 <169068345+Brunogabriel12@users.noreply.github.com>
2024-05-13 14:57:52 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("|---------------|");
+    System.out.println("|-- Fifit cent--|");
+    System.out.println("|_______________|");
+    
+    System.out.println("");
+
+    System.out.println("");
+
+    System.out.println("Álbum ");
+
+    System.out.println("");
+
+    System.out.println("Get Rich or Die Tryin' é o terceiro álbum do rapper 50 Cent lançado em parceria com a gravadora Interscope. O seu lançamento estava previsto para 11 de fevereiro de 2003, mas acabou sendo lançado uma semana antes, em 4 de fevereiro. Foi produzido em parceria de Eminem e Dr Dre, contém canções como In da Club, 21 Questions, P.I.M.P. e If I Can't. O álbum vendeu 18 milhões de cópias. Este álbum está na lista dos 200 álbuns definitivos no Rock and Roll Hall of Fame[2] ");
+
+    System.out.println("");
+
+     System.out.println("*Musicas*");
+     System.out.println("What Up Gangsta 2:59, atiently Waiting 4:48, Many Men (Wish Death");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
